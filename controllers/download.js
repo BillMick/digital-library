c3a21f3e4bbacfecb81d3fb0dee8cfde2ebbf340
Michaël A. <ahouandjinoubill@gmail.com>
2025-03-24 18:17:50 +0100

    not downloadable specification

--- controllers/download.js
+++ controllers/download.js
@@ -21,8 +21,11 @@ exports.Download = async (data, response) => {
                 const file = await prisma.file.findUnique({
                     where:{ id: parseInt(data.params.fileId), },
                 });
-                if (!file || !file.isAccessible || !file.isDownloadable) {
-                    return response.status(404).send("No such file.");
+                if (!file || !file.isAccessible) {
+                    return response.status(404).json({message: "No such file."});
+                }
+                else if (!file.isDownloadable) {
+                    return response.status(404).json({message: "Not authorized to download."});
                 }
             
                 const filePath = path.join(__dirname, "../", file.path);
@@ -41,10 +44,8 @@ exports.Download = async (data, response) => {
                         if (err) {
                             return response.status(500).json({ message: "Error downloading the file" });
                         }
-                        // return response.status(200).json({ status: true, file: file, });
                     });
                 });
-
             }
             else {
                 return response.status(403).json({ message: "You are not authorized.", });
