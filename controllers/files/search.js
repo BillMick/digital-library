80403f1c58bd04a3cf922db29dbb21f8ebf1078b
Michaël A. <ahouandjinoubill@gmail.com>
2025-03-27 08:10:24 +0100

    search operations by keys

---
+++ controllers/files/search.js
@@ -0,0 +1,93 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// ################# add userId to verify role (important for non accessible files...)
+
+exports.SearchByID = async (data, response) => {
+    let required_keys = ["fileId"];
+    const missing_keys = required_keys.filter((key) => !(key in data.params));
+    if (missing_keys.length > 0) {
+        return response.status(400).json({
+          error: "Bad Request.",
+          message: `Missing keys: ${missing_keys.join(", ")}`,
+        });
+    }
+    else {
+        const file = await prisma.file.findFirst({
+            where: {
+                id: data.params.id,
+                isAccessible: true,
+            },
+            include: {
+                categories: true,
+                tags: true,
+                ratings: true,
+                comments: true,
+            }
+        });
+        console.log(file);
+        response.status(200).json({
+            file: file
+        });
+    }
+}
+
+
+exports.SearchByTitle = async (data, response) => {
+    let required_keys = ["title"];
+    const missing_keys = required_keys.filter((key) => !(key in data.params));
+    if (missing_keys.length > 0) {
+        return response.status(400).json({
+          error: "Bad Request.",
+          message: `Missing keys: ${missing_keys.join(", ")}`,
+        });
+    }
+    else {
+        const file = await prisma.file.findMany({
+            where: {
+                title: data.params.title,
+                isAccessible: true,
+            },
+            include: {
+                categories: true,
+                tags: true,
+                ratings: true,
+                comments: true,
+            }
+        });
+        console.log(file);
+        response.status(200).json({
+            file: file
+        });
+    }
+}
+
+
+exports.SearchByAuthor = async (data, response) => {
+    let required_keys = ["author"];
+    const missing_keys = required_keys.filter((key) => !(key in data.params));
+    if (missing_keys.length > 0) {
+        return response.status(400).json({
+          error: "Bad Request.",
+          message: `Missing keys: ${missing_keys.join(", ")}`,
+        });
+    }
+    else {
+        const file = await prisma.file.findMany({
+            where: {
+                author: data.params.author,
+                isAccessible: true,
+            },
+            include: {
+                categories: true,
+                tags: true,
+                ratings: true,
+                comments: true,
+            }
+        });
+        console.log(file);
+        response.status(200).json({
+            file: file
+        });
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const checkUserValidity = require('../middlewares/user-validity');
 const upload_controller = require("../controllers/files/upload.js");
+const search_controller = require("../controllers/files/search.js");
 const download_controller = require("../controllers/files/download.js");
 const ragController = require("../controllers/rag/gemini_loading.js");
 
@@ -18,6 +19,10 @@ router.get("/generate", ragController.history);
 const multer = require("../middlewares/multer-config");
 router.post( "/upload", multer.single("file"), upload_controller.Upload );
 router.get("/download/:fileId", download_controller.Download);
+
+router.get("/by-id/:fileId", search_controller.SearchByID);
+router.get("/by-title/:title", search_controller.SearchByTitle);
+router.get("/by-author/:author", search_controller.SearchByAuthor);
 
 
 
