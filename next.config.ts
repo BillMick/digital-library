94e3b1edc9d9587f9912e260e1a8116d9cf5d669
Michaël A. <ahouandjinoubill@gmail.com>
2025-04-13 15:12:17 +0200

    Misc.

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,35 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "images.pexels.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//         port: "",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true, // Disable default image optimization
   },
+  assetPrefix: isProd ? '/digital-library/' : '',
+  basePath: isProd ? '/digital-library' : '',
+  output: 'export'
 };
 
 export default nextConfig;
